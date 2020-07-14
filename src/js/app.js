0d9ab030806577fe976cce3e629b944ea1b487b9
mei-riko <dobrovolska.marina@gmail.com>
2020-07-14 16:36:02 +0300

    check text&sidebar&error

--- src/js/app.js
+++ src/js/app.js
@@ -1,51 +1,61 @@
 import $ from 'jquery'
 $(document).ready(() =>{
-    $("a.scroll").click(function() {
-        $("html, body").animate({
-           scrollTop: $($(this).attr("href")).offset().top + "px"
-        }, {
-           duration: 500,
-           easing: "swing"
-        });
-        return false;
+  $("a.scroll").click(function() {
+    $("html, body").animate({
+       scrollTop: $($(this).attr("href")).offset().top + "px"
+    }, {
+       duration: 500,
+       easing: "swing"
     });
-	
-	// Input mask
-	if( $('.phone').length > 0 ) {
-	$(".phone").inputmask({
-	  mask: "8 999 999 99 99",
-	  placeholder: " ",
-	  showMaskOnHover: true,
-	  onincomplete: function(){ 
-		$(this).closest("form").addClass('error-phone'); 
-		$(this).addClass('error'); 
-		$(this).siblings(".error_phone").addClass('error').html('Укажите корректный номер'); 
-	  }, 
-	  oncomplete: function(){ 
-		  $(this).closest("form").removeClass('error-phone'); 
-		  $(this).removeClass('error'); 
-		  $(this).siblings(".error_phone").removeClass('error').html(''); 
-	  },
-	})
-	}
-	$('input.phone').on('keydown', function(event) {
-	  if (event.keyCode === 13 && !$(this).inputmask("isComplete") ) {
-		  event.preventDefault();
-		  $(this).blur();
-		  return false;
-	  }
-	});
-	
+    return false;
+  });
+
+  // Input mask
+  if( $('.phone').length > 0 ) {
+    $(".phone").inputmask({
+      mask: "+7 999 999 99 99",
+      placeholder: " ",
+      showMaskOnHover: true,
+
+      onincomplete: function(){ 
+        $(this).closest("form").addClass('error-phone'); 
+        $(this).addClass('error'); 
+        $(this).siblings(".error_phone").addClass('error').html('Укажите корректный номер'); 
+      }, 
+      oncomplete: function(){ 
+        $(this).closest("form").removeClass('error-phone'); 
+        $(this).removeClass('error'); 
+        $(this).siblings(".error_phone").removeClass('error').html(''); 
+      },
+    })
+  }
+  $('input.phone').on('keydown', function(event) {
+    if (event.keyCode === 13 && !$(this).inputmask("isComplete") ) {
+      event.preventDefault();
+      $(this).blur();
+      return false;
+    }
+  });
+
+  // Modal
+  $('[data-fancybox="modal"]').fancybox({
+    autoFocus: false
+  });
+
+  // Отмена стандартного поведения ссылки
+  $('a[data-trigger="click"]').click(function(e){
+    e.preventDefault();
+  })
 	  // Mobile Navbar
   $(".navbar-toggle#nav").on("click", function(){
     if( !$(this).hasClass("navbar-toggle--active")){
         $(this).addClass("navbar-toggle--active");
-        $(".navbar-mobile").addClass("navbar-mobile--active");
-        $(".navbar-mobile").slideDown();
+        // $(".navbar-mobile").addClass("navbar-mobile--active");
+        // $(".navbar-mobile").slideDown();
     }else{
         $(this).removeClass("navbar-toggle--active");
-        $(".navbar-mobile").removeClass("navbar-mobile--active");
-        $(".navbar-mobile").slideUp();
+        // $(".navbar-mobile").removeClass("navbar-mobile--active");
+        // $(".navbar-mobile").slideUp();
     }
   });
   $(document).mouseup(function (e){ // событие клика по веб-документу
@@ -63,8 +73,8 @@ $(document).ready(() =>{
   $(window).resize(function(){
     if ( $(window).width() > 991 || !window.matchMedia('screen and (max-width: 992px)').matches ){
       $(".navbar-toggle").removeClass("navbar-toggle--active");
-      $(".navbar-mobile.navbar-mobile--active").removeClass("navbar-mobile--active");
-      $(".navbar-mobile").slideUp();
+      // $(".navbar-mobile.navbar-mobile--active").removeClass("navbar-mobile--active");
+      // $(".navbar-mobile").slideUp();
     }
   });
 });
